616d5d48bd56f4dd47e57b0c1c812b711e162c81
Elissa Ross <elissa.ross@meshconsultants.ca>
2017-11-10 13:27:51 -0500

    added the Urho icon, builds Xcode proj now

--- cmake_elissa.sh
+++ cmake_elissa.sh
@@ -1,1 +1,1 @@
-cmake -BBuild/gcc_build -H. -DURHO3D_HOME="~/Dev/Urho3D_Build/" -DURHO3D_C++11=1 -DURHO3D_OPENGL=1 -DURHO3D_64BIT=1 -G Xcode -DIOGRAM_SDK="~/Dev/IogramEditor/Source/"
+cmake -BBuild/gcc_build -H. -DURHO3D_HOME="~/Dev/Urho3D_Build/" -DURHO3D_C++11=1 -DURHO3D_OPENGL=1 -DURHO3D_64BIT=1 -G Xcode -DIOGRAM_SDK="~/Dev/IogramEditor/Source/" 
