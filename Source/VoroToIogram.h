c88c4f40215776acbf676552a3419a595bed605e
Elissa Ross <elissa.ross@meshconsultants.ca>
2017-11-13 14:58:21 -0500

    trying to get symbols to export

--- Source/VoroToIogram.h
+++ Source/VoroToIogram.h
@@ -2,10 +2,17 @@
 #include "voro++.hh"
 #include <Urho3D/Core/Variant.h>
 
-// conversion from voro++ voronoi cells to trimesh output
 
+
+// conversion from voro++ voronoi cells to trimesh output
+#define EXPORT __attribute__((visibility("default")))
+
+EXPORT
 Urho3D::Variant GetMeshFromVoronoiCell(voro::voronoicell &c, double x, double y, double z);
 
 Urho3D::VariantVector GetMeshesFromContainer(voro::container &con);
 
 Urho3D::VariantVector draw_cells_iogram(voro::c_loop_all &vl, voro::container &con);
+
+
+
